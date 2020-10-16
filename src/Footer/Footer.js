56c3a6900eb364c1b23d225f5565c59ea89a0d1e
Pranitha <amruthapranitha016@gmail.com>
2020-10-15 22:39:14 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer aria-label="Content Info">
+    <div className="center">
+        All rights reserved &copy; Fabio Nolasco
+    </div>
+</footer>
+  );
+}
+
+export default Footer;
