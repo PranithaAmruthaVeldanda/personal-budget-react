ab74123a98cb7bcfe5a977fae36bdf6138e0f3a9
Pranitha <amruthapranitha016@gmail.com>
2020-10-16 01:36:14 -0400

    Pie chart

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -10,7 +10,7 @@ function HomePage() {
         <div className="page-area" id="maincontent">
                       
             <article>
-                <h1>Stay on track</h1>
+                <h2>Stay on track</h2>
                 <p>
                     Do you know where you are spending your money? If you really stop to track it down,
                     you would get surprised! Proper budget management depends on real data... and this
@@ -19,14 +19,14 @@ function HomePage() {
             </article>
              
             <article>            
-                <h1>Alerts</h1>
+                <h2>Alerts</h2>
                 <p>
                     What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
                 </p>            
             </article>
 
             <article>
-                <h1>Results</h1>
+                <h2>Results</h2>
                 <p>
                     People who stick to a financial plan, budgeting every expense, get out of debt faster!
                     Also, they to live happier lives... since they expend without guilt or fear... 
@@ -36,42 +36,15 @@ function HomePage() {
                
 
             <article>
-                <h1>Free</h1>
+                <h2>Free</h2>
                 <p>
                     This app is free!!! And you are the only one holding your data!
-                </p>
-            </article>
-
-
-            <article>
-                <h1>Stay on track</h1>
-                <p>
-                    Do you know where you are spending your money? If you really stop to track it down,
-                    you would get surprised! Proper budget management depends on real data... and this
-                    app will help you with that!
-                </p>
-            </article>
-    
-            <article>
-                <h1>Alerts</h1>
-                <p>
-                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
-                </p>
-            </article>
-    
-            
-            <article>
-                <h1>Results</h1>
-                <p>
-                    People who stick to a financial plan, budgeting every expense, get out of debt faster!
-                    Also, they to live happier lives... since they expend without guilt or fear... 
-                    because they know it is all good and accounted for.
                 </p>
             </article>
     
            
             <article>
-                <h1><center>Pie Chart using Axios</center></h1>
+                <h2>Pie Chart using Axios</h2>
                 <p style={{height:"500px",width:"500px"}}>
                     <AxiosPie />
                 </p>
